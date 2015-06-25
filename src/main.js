5571640bb1f5e7e710197f4e6021fd7e4bd68261
HyeonuPark <nemo1275@gmail.com>
2015-06-25 16:46:29 +0900

    add context filter

--- src/main.js
+++ src/main.js
@@ -1,28 +1,84 @@
 var PromiseEmitter = require('promise-emitter');
+var uuid = require('uuid').v4;
 
 function Pipe() {
+    var upEmitterDefault, downEmitterDefault;
     var upEmitters = {};
     var downEmitters = {};
+    var upPromises = {};
+    var downPromises = {};
     this.top = {
-        on : function(event) {
-            if (!upEmitters[event]) upEmitters[event] = new PromiseEmitter();
-            return upEmitters[event];
+        on : function(topic) {
+            if (!topic) {
+                upEmitterDefault = new PromiseEmitter();
+                return upEmitterDefault;
+            }
+            upEmitters[topic] = new PromiseEmitter();
+            return upEmitters[topic];
         },
-        send : function(event, data) {
-            if (downEmitters[event]) return downEmitters[event].emit(data);
-            else return Promise.resolve();
+        send : function(topic, data, prevEvent, timeToLive) {
+            data._topic = topic;
+            data._context = (prevEvent && prevEvent._context) || uuid();
+            if (downPromises[data._context]) {
+                downPromises[data._context](data);
+                delete downPromises[data._context];
+            } else if (downEmitters[topic]) {
+                downEmitters[topic].emit(data);
+            } else {
+                downEmitterDefault && downEmitterDefault.emit(data);
+            }
+
+            if (timeToLive && timeToLive < 0) return Promise.resolve();
+
+            return new Promise(function(res, rej) {
+                timeToLive = timeToLive || Pipe.defaultTimeToLive;
+                upPromises[data._context] = res;
+                setTimeout(function() {
+                    if (upPromises[data._context]) {
+                        delete upPromises[data._context];
+                        rej(new Error('Response time out'));
+                    }
+                }, timeToLive);
+            });
         }
     };
     this.bottom = {
-        on : function(event) {
-            if (!downEmitters[event]) downEmitters[event] = new PromiseEmitter();
-            return downEmitters[event];
+        on : function(topic) {
+            if (!topic) {
+                downEmitterDefault = new PromiseEmitter();
+                return downEmitterDefault;
+            }
+            downEmitters[topic] = new PromiseEmitter();
+            return downEmitters[topic];
         },
-        send : function(event, data) {
-            if (upEmitters[event]) return upEmitters[event].emit(data);
-            else return Promise.resolve();
+        send : function(topic, data, prevEvent, timeToLive) {
+            data._topic = topic;
+            data._context = (prevEvent && prevEvent._context) || uuid();
+            if (upPromises[data._context]) {
+                upPromises[data._context](data);
+                delete upPromises[data._context];
+            } else if (upEmitters[topic]) {
+                upEmitters[topic].emit(data);
+            } else {
+                upEmitterDefault && upEmitterDefault.emit(data);
+            }
+
+            if (timeToLive && timeToLive < 0) return Promise.resolve();
+
+            return new Promise(function(res, rej) {
+                timeToLive = timeToLive || Pipe.defaultTimeToLive;
+                downPromises[data._context] = res;
+                setTimeout(function() {
+                    if (downPromises[data._context]) {
+                        delete downPromises[data._context];
+                        rej(new Error('Response time out'));
+                    }
+                }, timeToLive);
+            });
         }
-    }
+    };
 }
+
+Pipe.defaultTimeToLive = 10000;
 
 module.exports = Pipe;
